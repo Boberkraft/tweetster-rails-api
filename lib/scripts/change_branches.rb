dd7f90862927d6b0e749fc8b8d71010b35f1e1d2
Boberkraft <andrzej.bisewski@gmail.com>
2020-06-26 16:44:35 +0200

    x

--- lib/scripts/change_branches.rb
+++ lib/scripts/change_branches.rb
@@ -13,6 +13,7 @@ def switch_branches
   REPOS.each do |repo|
     puts ENV.to_h
     puts `tree #{ENV['GITHUB_WORKSPACE']}`
+    puts File.join(ENV['GITHUB_WORKSPACE'], repo)
     `cd #{File.join(ENV['GITHUB_WORKSPACE'], repo)}`
     `git checkout #{ENV['BRANCH_NAME']}` if remote_branch_exists?
   end
