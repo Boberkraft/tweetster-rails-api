b4653c9a3e49f6a0b0214ed2ac651aaff74f6e06
Boberkraft <andrzej.bisewski@gmail.com>
2020-07-12 13:36:26 +0200

    fix chaning branches

--- lib/workflow/change_branches.rb
+++ lib/workflow/change_branches.rb
@@ -4,17 +4,27 @@ require 'json'
 
 REPOS = %w[tweetster-rails-api chess Rails-4-Twitter-Clone]
 
+$branch = ENV['BRANCH_NAME']
+
 def remote_branch_exists?
-  branch = ENV['BRANCH_NAME']
-  system("git branch --list #{branch}| grep #{branch}")
+  system("git branch --list #{$branch}| grep #{$branch}")
+end
+
+def change_branch_cmd
+  if remote_branch_exists?
+    'git checkout $branch && git pull'
+  else
+    'cat ":sad:"'
+  end
+end
+
+def cd_repo(repo)
+  "cd #{File.join(ENV['GITHUB_WORKSPACE'], repo)}"
 end
 
 def switch_branches
   REPOS.each do |repo|
-    puts ENV.to_h
-    puts `tree #{ENV['GITHUB_WORKSPACE']}`
-    puts File.join(ENV['GITHUB_WORKSPACE'], repo)
-    system("#{File.join(ENV['GITHUB_WORKSPACE'], repo)} && #{remote_branch_exists? ? ENV['BRANCH_NAME'] : 'cat "a"' }")
+    system("#{cd_repo(repo)} && #{change_branch_cmd}")
   end
 end
 
